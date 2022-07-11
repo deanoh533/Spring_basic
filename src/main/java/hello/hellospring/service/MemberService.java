8b1f3f4625c08ef3c0869a565065014045757c46
deanoh533 <deanoh533@gmail.com>
2022-07-11 13:51:05 +0900

    Config 자바 코드로 의존관계 설정

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service //@Component를 포함
+//@Service //@Component를 포함
 public class MemberService {
     private final MemberRepository memberRepository ;
-    @Autowired
+    //@Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
