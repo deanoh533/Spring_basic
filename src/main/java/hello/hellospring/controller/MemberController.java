a05c11520f204e2a58dfba1f18721c49c4c7e50d
deanoh533 <deanoh533@gmail.com>
2022-07-11 23:14:34 +0900

    회원 웹 기능 등록

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,9 +1,12 @@
 package hello.hellospring.controller;
 
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 //스프링 컨테이너가 MemberController의 객체를 만들어서 관리해준다. (=빈에 등록된다.)
 @Controller //@Component 포함
@@ -15,6 +18,23 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+
+
 }
 /**
  * controller   -> Service  -> Repository 의 연관관게
